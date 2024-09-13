a52dba95e51803ac4eaf852227c3e4da57ca53e3
aikunaaa <143726710+aikunaaa@users.noreply.github.com>
2024-09-13 21:22:12 +0800

    Add files via upload

---
+++ ez_php/src/index.php
@@ -0,0 +1,10 @@
+<?php
+ini_set("max_execution_time", "180");
+show_source(__FILE__);
+include('flag.php');
+$a= $_GET["a"];
+if(isset($a)&&(file_get_contents($a,'r')) === 'I want flag'){
+	echo "success\n";
+	echo $flag;
+}
+?>
